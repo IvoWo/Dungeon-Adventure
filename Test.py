d7b072fd117c5edc1a888444c454131d450497a8
Kerr206011 <paul.fach6@gmail.com>
2023-06-30 12:54:52 +0200

    Itemliste
    Itemliste in Room

--- Test.py
+++ Test.py
@@ -2,6 +2,8 @@ from Game import *
 
 StartRoom = Room()
 Knife = Item("Knife", "usefull to cut stuff")
+Pan = Item("Pan", "usefull to cook stuff")
 Player1 = Player(Room)
 Player1.collectItem(Knife)
+Player1.collectItem(Pan)
 print(Player1.inspectInventory())
