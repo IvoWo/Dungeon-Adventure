b0d49453db2190de53928ec823edc0198afc48eb
Kerr206011 <paul.fach6@gmail.com>
2023-07-04 18:28:04 +0200

    BUTTONS!!!!!
    There are now buttons to use to your hearts desire.

--- Game.py
+++ Game.py
@@ -9,6 +9,18 @@ pygame.display.set_caption('Dungeon-Adventure')
 clock = pygame.time.Clock()
 PauseGame = False
 
+#define Fonts
+font = pygame.font.SysFont("arialblack", 40)
+
+#define colors
+TEXT_COL = (255, 255, 255)
+
+def drawText(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+Start_img = pygame.image.load('pictures/Start_Button.jpg').convert_alpha()
+start_button = Button(50, 240, Start_img, 0.1)
 
 # instanciate groups
 #Groups
@@ -22,6 +34,11 @@ background_surf = pygame.transform.rotozoom(pygame.image.load('pictures/blackBac
 
 # game loop
 while True:
+
+    #wenn der Startbutton gedrückt wird mach was
+    if start_button.draw(screen):
+        PauseGame = not PauseGame
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -30,6 +47,7 @@ while True:
             PauseGame = not PauseGame
 
     if PauseGame:
+        drawText("press Space to continue", font, TEXT_COL, 50, 250)
         pass
     else:
         
